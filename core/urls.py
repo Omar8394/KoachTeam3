dfd0eb6f66b4b4073a601fb48b91d20fb8b76d07
Nix_localhost <analista0093@gmail.com>
2021-10-01 17:01:09 +0200

    cambios nixon

--- core/urls.py
+++ core/urls.py
@@ -14,7 +14,9 @@ urlpatterns = [
     path("academic/", include("modules.academic.urls")),
     path("communication/", include("modules.communication.urls")),
     path("planning/", include("modules.planning.urls")),
+    path("help_app/", include("modules.helping.urls")),
     path("", include("modules.security.urls")),
+    
                                                          # Auth routes - login / register
     path('admin/', admin.site.urls),                     # Django admin route
     path("", include("modules.app.urls")),               # UI Kits Html files
