33c003c43ab749f1acf1e4a581662bc8b1823649
Freddy Ramos <tadifred@gmail.com>
2021-10-05 00:09:43 -0400

    Agregada la vista full registration html, cambios en el modelos security. cambios en el login

--- modules/security/views.py
+++ modules/security/views.py
@@ -13,31 +13,60 @@ from django.forms.utils import ErrorList
 from django.http import HttpResponse
 from .forms import LoginForm, SignUpForm, ResetPasswordForm, LandingPage
 from modules.app.models import TablasConfiguracion
+from modules.security.models import CtaUsuario
+from modules.security import Methods
+
 
 def login_view(request):
-    form = LoginForm(request.POST or None)
+    if request.user.is_authenticated:
+        return redirect("/")
+    else:
+        form = LoginForm(request.POST or None)
 
-    msg = None
+        msg = None
 
-    if request.method == "POST":
+        if request.method == "POST":
 
-        if form.is_valid():
-            username = form.cleaned_data.get("username")
-            password = form.cleaned_data.get("password")
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect("/")
-            else:    
-                msg = 'Invalid credentials'    
-        else:
-            msg = 'Error validating the form'    
+            if form.is_valid():
+                #tools = Methods.securityTools()
+                username = form.cleaned_data.get("username")
+                password = form.cleaned_data.get("password")
+               # cuenta = CtaUsuario.objects.filter(codigo_cta=username)[0]
+                #cuenta = CtaUsuario.objects.get(cuenta.idcta_usuario)
+                # traer preferencias de usuario para comparar atributos
+                # bajo cualquier caso registrar en el log
+                """if cuenta:
+                    if cuenta.fk_status_cuenta == 0:  # ejemplo cambiar a valor y verificar clave foranea
+                        # usuario bloqueado
+                        # redirigir a pantalla bloqueo
+                        pass
+                    elif cuenta.intentos_fallidos < 3:  # depende de las preferecnias del usuario
 
-    return render(request, "security/login.html", {"form": form, "msg" : msg})
+                        pass
+                    elif cuenta.fecha_ult_cambio is None:
+                        # primer ingreso debe cambiar contraseña si la cuenta fue creada
+                        # por el administrador
+                        pass
+                    elif tools.exp_clave(cuenta.fecha_ult_cambio, cuenta.dias_cambio):
+                        # redireccionar a pantalla de cambiar clave
+                        pass
+                    else:"""
+                user = authenticate(username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    return redirect("/")
+                else:
+                    msg = 'Invalid credentials'
+                            # registrar intentos fallidos.
+                            # verificar si la cantidad de intentos es igual a la maxima y bloquear usuario
+                            # registrar en el log
+            else:
+                msg = 'Error validating the form'
+        return render(request, "security/login.html", {"form": form, "msg": msg})
+
 
 def register_user(request):
-
-    msg     = None
+    msg = None
     success = False
 
     if request.method == "POST":
@@ -47,22 +76,20 @@ def register_user(request):
             username = form.cleaned_data.get("username")
             raw_password = form.cleaned_data.get("password1")
             user = authenticate(username=username, password=raw_password)
-
-            msg     = 'User created - please <a href="/login">login</a>.'
+            msg = 'User created - please <a href="/login">login</a>.'
             success = True
-            
-            #return redirect("/login/")
+            return redirect("/login/")
 
         else:
-            msg = 'Form is not valid'    
+            msg = 'Form is not valid'
     else:
         form = SignUpForm()
 
-    return render(request, "security/register.html", {"form": form, "msg" : msg, "success" : success })
+    return render(request, "security/register.html", {"form": form, "msg": msg, "success": success})
+
 
 def forgot_password(request):
-
-    msg     = None
+    msg = None
     success = False
 
     if request.method == "POST":
@@ -70,16 +97,17 @@ def forgot_password(request):
         if form.is_valid():
             email = form.cleaned_data.get("email")
 
-            msg     = 'Password reset email sent! - please check your email inbox'
+            msg = 'Password reset email sent! - please check your email inbox'
             success = True
-            
-            #return redirect("/login/")
+
+            # return redirect("/login/")
         else:
-            msg = 'Form is not valid'    
+            msg = 'Form is not valid'
     else:
         form = ResetPasswordForm()
 
-    return render(request, "security/passwordReset.html", {"form": form, "msg" : msg, "success" : success })
+    return render(request, "security/passwordReset.html", {"form": form, "msg": msg, "success": success})
+
 
 def lang_page(request):
     form = LandingPage(request.POST or None)
@@ -87,15 +115,15 @@ def lang_page(request):
     success = False
     tipo_telefono = None
     if request.method == "POST":
-            if form.is_valid():
-                print(form)
-                form.save()
-                success = True
-                msg = "tu solicitud ha sido enviada."
-            else:
-                print(form)
-                tipo_telefono = TablasConfiguracion.obtenerHijos("tipTelefono")
-                msg = 'Error validating the form'
+        if form.is_valid():
+            print(form)
+            form.save()
+            success = True
+            msg = "Your access request has been sent."
+        else:
+            print(form)
+            tipo_telefono = TablasConfiguracion.obtenerHijos("tipTelefono")
+            msg = 'Error validating the form'
     else:
         tipo_telefono = TablasConfiguracion.obtenerHijos("tipTelefono")
         form = LandingPage()
@@ -103,3 +131,26 @@ def lang_page(request):
 
     return render(request, "security/landPage.html", {"form": form, "msg": msg, 'success': success,
                                                       "tipoTelefono": tipo_telefono})
+
+def full_registration(request):
+    msg = None
+    success = False
+    form_land_page = LandingPage(request.POST)
+    form_registration = SignUpForm(request.POST)
+    tipo_telefono = None
+    if request.method == "POST":
+        print("form lanpage:", form_land_page)
+        print("form registro:", form_registration)
+        if form_land_page.is_valid() and form_registration.is_valid():
+            form_land_page.save()
+            form_registration.save()
+            success = True
+            msg = "Registration has been completed successfully."
+        else:
+            tipo_telefono = TablasConfiguracion.obtenerHijos("tipTelefono")
+            msg = 'Error validating the form'
+    else:
+        tipo_telefono = TablasConfiguracion.obtenerHijos("tipTelefono")
+        form_land_page = LandingPage()
+        form_registration = SignUpForm()
+    return render(request, "security/full_registration.html", {"msg": msg, 'success': success, "tipoTelefono": tipo_telefono})
