2f34f3c4bc9b6b335966c5333f11db9143b08a49
alpadel163 <koachsolucionesintegrales@gmail.com>
2021-10-26 16:11:27 -0400

    landingpage aprobar solicitudes
    landing omar

--- modules/helping/urls.py
+++ modules/helping/urls.py
@@ -4,8 +4,8 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.urls import path, re_path
-from django.urls import path  
-from modules.helping.views import save_Q,showhelps,helpsdelete,helpsedit,show_questions,questionask  
+from django.urls import path
+from modules.helping.views import save_Q,showhelps,helpsdelete,helpsedit,show_questions,questionask,landingshow, sending 
 from modules.helping import views
 
 
@@ -16,6 +16,8 @@ urlpatterns = [
     path('deletehelp/<int:id>',helpsdelete, name="deletehelp"),
     path('showQuestions/',show_questions,name="show_questions"),
     path('showAnswers/<int:id>',questionask,name="showanswers"),
+    path('landingshow/',landingshow,name="landingshow"),
+    path('sending/',sending,name="sending"),
     path('helpSecurity/', views.helpSecurity, name="helpSecurity"),   
     path('myHelp/', views.myHelp, name="myHelp"),   
 ]
