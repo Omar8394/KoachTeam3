8a74022253c0d305012df289452cf9b3bbe5bc06
Chemi Martinez <jmmartineztuarez@gmail.com>
2021-10-30 19:51:13 -0400

    parche para helping
    -añade pequeño parche para las url de helping y la sidebar

--- modules/helping/urls.py
+++ modules/helping/urls.py
@@ -5,7 +5,7 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django.urls import path, re_path
 from django.urls import path  
-from modules.helping.views import save_Q,showhelps,helpsdelete,helpsedit,show_questions,questionask,landingshow, sending   
+from modules.helping.views import save_Q,showhelps,helpsdelete,helpsedit,show_questions,questionask  
 from modules.helping import views
 
 
@@ -25,8 +25,8 @@ urlpatterns = [
     path('deletehelp/<int:id>',helpsdelete, name="deletehelp"),
     path('showQuestions/',show_questions,name="show_questions"),
     path('showAnswers/<int:id>',questionask,name="showanswers"),
-    path('landingshow/',landingshow,name="landingshow"),
-    path('sending/',sending,name="sending"),
+    # path('landingshow/',landingshow,name="landingshow"),
+    # path('sending/',sending,name="sending"),
     path('helpSecurity/', views.helpSecurity, name="helpSecurity"),   
     path('myHelp/', views.myHelp, name="myHelp"),   
 ]
